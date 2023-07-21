8071d45bb0de3d23c2705f6353a8b455dceb5368
jk1791 <140122725+jk1791@users.noreply.github.com>
2023-07-22 01:49:01 +0200

    Add files via upload

--- scripts/stochastic/plot_ts.py
+++ scripts/stochastic/plot_ts.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 # -------------------------------------------------------------- #
 
-print("\n     plot time series (20.07.2023)\n\n")
-
+#    plot time series (20.07.2023)
+#
 #    INPUT PARAMETERS:
 
 # input file
-infile = "levy_stable_alpha1.5_T100k.dat"
+infile = "pareto_signed_b6.0_T100k.dat"
 # number of columns
 num_col = 1
 # time series column
